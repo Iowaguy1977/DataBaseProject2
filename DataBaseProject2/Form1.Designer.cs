68650fa19fbdca5877cbe144bd28264ae19618f0
chris kalkas <chris.kalkas@icloud.com>
2025-06-19 11:42:56 -0500

    update xml

--- DataBaseProject2/Form1.Designer.cs
+++ DataBaseProject2/Form1.Designer.cs
@@ -30,6 +30,18 @@
         {
             button1 = new Button();
             button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -46,7 +58,7 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(318, 219);
+            button2.Location = new Point(327, 219);
             button2.Name = "button2";
             button2.Size = new Size(131, 34);
             button2.TabIndex = 1;
@@ -54,21 +66,145 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(129, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(129, 74);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(129, 131);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(358, 28);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 5;
+            textBox4.KeyPress += textBox4_KeyPress;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(358, 74);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(358, 131);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 7;
+            textBox6.KeyPress += textBox6_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Order_ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 9;
+            label2.Text = "ITem_Type";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(29, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Item_Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(281, 36);
+            label4.Name = "label4";
+            label4.Size = new Size(26, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Qty";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(268, 82);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 15);
+            label5.TabIndex = 12;
+            label5.Text = "Price_Each";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(268, 139);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Customer_ID";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
--- DataBaseProject2/Form1.cs
+++ DataBaseProject2/Form1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.IO;
 using System.Xml.Serialization;
 using Dapper;
@@ -20,29 +21,57 @@ namespace DataBaseProject2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double result;
+            if (!double.TryParse(textBox5.Text, out result))
+            {
+                MessageBox.Show("Please enter a valid price for Price Each.");
+                return;
+            }
 
             string sql = "INSERT INTO Orders (Item_Type,Item_Name,Qty,Price_Each,Customer_ID) VALUES (@Item_Type,@Item_Name, @Qty, @Price_Each, @Customer_ID)";
             string filepath = "C:\\Users\\chris\\source\\repos\\DataBaseProject2\\DatabaseProject2.xml.txt";
             XmlSerializer serializer = new XmlSerializer(typeof(Orders));
             using (FileStream fs = new FileStream(filepath, FileMode.Open))
             {
-                Orders orders = (Orders)serializer.Deserialize(fs); 
+                Orders orders = (Orders)serializer.Deserialize(fs);
+                orders.orderlist.Add(new order { Order_ID = Convert.ToInt32(textBox1.Text), Item_Type = textBox2.Text, Item_Name = textBox3.Text, Qty = Convert.ToInt32(textBox4.Text), Price_Each = Convert.ToDouble(textBox5.Text), Customer_ID = Convert.ToInt32(textBox6.Text) });
 
 
-                for (int i = 0; i < orders.orderlist.Count; i++)
-                {
 
-                    MessageBox.Show(orders.orderlist.ElementAt(i).Item_Type);
-                }
                 using (var connection = new SqlConnection(connectionString))
                 {
-                    for (int i = 0; i < orders.orderlist.Count; i++)
+                    MessageBoxButtons buttons = MessageBoxButtons.OKCancel;
+                    MessageBox.Show("Are you sure you want to add this order?", "Confirmation", buttons);
+                    if (buttons.Equals(DialogResult.Cancel))
+                    {
+                        return;
+                        { connection.Close(); }
+
+                    }
+                    else
                     {
                         connection.Open();
-                        _ = connection.Execute(sql, new { Item_Type = orders.orderlist.ElementAt(i).Item_Type, Item_Name = orders.orderlist.ElementAt(i).Item_Name, Qty = orders.orderlist.ElementAt(i).Qty, Price_Each = orders.orderlist.ElementAt(i).Price_Each, Customer_ID = orders.orderlist.ElementAt(i).Customer_ID });
+                        _ = connection.Execute(sql, new { Item_Type = orders.orderlist.ElementAt(orders.orderlist.Count - 1).Item_Type, Item_Name = orders.orderlist.ElementAt(orders.orderlist.Count - 1).Item_Name, Qty = orders.orderlist.ElementAt(orders.orderlist.Count - 1).Qty, Price_Each = orders.orderlist.ElementAt(orders.orderlist.Count - 1).Price_Each, Customer_ID = orders.orderlist.ElementAt(orders.orderlist.Count - 1).Customer_ID });
                         connection.Close();
                     }
+
+
+
                 }
+            }
+            String pulleddata = "select * from Orders for XML Auto, ROOT('Orders'),ELEMENTS";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(pulleddata, conn))
+                {
+                    conn.Open();
+                    String Output = (String)cmd.ExecuteScalar();
+                    File.WriteAllText("C:\\Users\\chris\\source\\repos\\DataBaseProject2\\DatabaseProject2.xml.txt", Output);
+
+                    conn.Close();
+                }
+
+                MessageBox.Show("Textfile Created successfully!");
             }
 
 
@@ -52,32 +81,47 @@ namespace DataBaseProject2
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string sql = "Select * from Orders ";
-            string connectionstring = "C:\\Users\\chris\\source\\repos\\DataBaseProject2\\DatabaseProject2.xml.txt";
-            XmlSerializer serializer = new XmlSerializer(typeof(Orders));
-            using (TextWriter fs = new StreamWriter(connectionstring))
+            Orders orders = new Orders();
+            String pulleddata = "select * from Orders for XML Auto, ROOT('Orders'),ELEMENTS";
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                Orders orders = new Orders();
-
-
-                serializer.Serialize(fs, orders);
-
-
-                for (int i = 0; i < orders.orderlist.Count; i++)
+                using (SqlCommand cmd = new SqlCommand(pulleddata, conn))
                 {
+                    conn.Open();
+                    String Output = (String)cmd.ExecuteScalar();
+                    File.WriteAllText("C:\\Users\\chris\\source\\repos\\DataBaseProject2\\DatabaseProject2.xml.txt", Output);
 
-                    MessageBox.Show(orders.orderlist.ElementAt(i).Item_Type);
+                    conn.Close();
                 }
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    var orders1 = connection.Query<Orders>(sql);
-                    SqlDataAdapter da = new SqlDataAdapter(sql, connection);
 
 
+            }
+        }
 
-                    connection.Close();
-                }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true; // Prevents the character from being entered
+            }
+        }
+
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true; // Prevents the character from being entered
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true; // Prevents the character from being entered
             }
         }
     }
